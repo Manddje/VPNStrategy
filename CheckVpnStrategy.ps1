11a6f8205b66f96fbe606582502def80bec7296c
Manddje <kris@mandemaker.info>
2022-02-16 10:35:08 +0100

    Add files via upload

---
+++ CheckVpnStrategy.ps1
@@ -0,0 +1,21 @@
+﻿$users = Get-ChildItem (Join-Path -Path $env:SystemDrive -ChildPath 'Users') -Exclude 'Public', '_adm*', 'ADMINI~*'
+if ($null -ne $users) {
+    foreach ($user in $users) {
+        $progPath = Join-Path -Path $user.FullName -ChildPath "AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk" 
+        
+        # Check if VPNStrategy is set to 14
+        $VpnStrategy = get-content $progPath
+        
+        if ($VpnStrategy -like "*VpnStrategy=14*"){
+        Write-Output "Correct value"
+        echo $progPath
+        exit 0
+        }
+   Else 
+        {
+        Write-Output "Wrong value"
+        echo $progPath
+        exit 1
+        }
+    }
+}
